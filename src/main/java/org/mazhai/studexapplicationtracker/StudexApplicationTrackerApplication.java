8150a614448fa8da88d7576980723fadfb7b7cfc
Jerry Jenith <jerryjenithj@gmail.com>
2022-04-02 15:17:18 +0530

    initialized

---
+++ src/main/java/org/mazhai/studexapplicationtracker/StudexApplicationTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.mazhai.studexapplicationtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudexApplicationTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudexApplicationTrackerApplication.class, args);
+	}
+
+}
